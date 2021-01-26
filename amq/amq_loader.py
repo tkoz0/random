3787196c5b459183b9b84d3e1e08b2b983973595
TKoz Zero <tkozlowski1999@gmail.com>
2021-01-26 06:20:43 -0500

    rewritten to support the rewrite of the scraper scrypt

--- amq/amq_loader.py
+++ amq/amq_loader.py
@@ -1,54 +1,70 @@
-# read all json files and load them
-# import this file and call get_data()
+'''
+Reads all the files from:
+ranked_data/amq_<year>s<season>_<day>_<date>_<region>_.json
 
+Produces an object containing all the ranked AMQ data stored.
+'''
+
+import bz2 # significantly better than gzip but not very slow
 import json
 import os
+import pickle
 import re
 
-dir_re = re.compile(r'[0-9]{4}_S[0-9]')
+re_fname = re.compile(r'amq_(\d{4})s(\d\d)_(ch|\d\d)_(\d{4})-(\d\d)-(\d\d)_'
+                       '(east|central|west)\.json')
 
-# amq data as a list of items formatted like this:
-# {
-#   "region": "western"
-#   "year": 2020
-#   "season": 11
-#   "number": 20 (-1 for championship)
-#   "date": "2020-11-21"
-#   "data": (object from the file on disk)
-# }
-def get_data(output=False):
+def read_ranked_data(use_cached_obj=False,store_cached_obj=True):
+    '''
+    Returns a list containing ranked objects (type dict) that look like:
+    {
+        "region": "western"
+        "year": 2020
+        "season": 11
+        "number": 20 # -1 for championship to keep the type consistent
+        "date": "2020-11-21"
+        "data": [object read from the JSON file on disk]
+    }
+    May store/read the data from "ranked_data.pickle" to speed things up
+    depending on the options provided
+    '''
+    if use_cached_obj and os.path.isfile('ranked_data.pickle.bz2'):
+        data = pickle.load(bz2.BZ2File('ranked_data.pickle.bz2','rb'))
+        return data
+    
+    filelist = os.listdir('ranked_data')
     data = []
-    dirs = list(filter(lambda x : dir_re.match(x), os.listdir('.')))
-    for dir in dirs:
-        files = os.listdir(dir)
-        for file in files:
-            if output: print('loading:',file)
-            # get year,season,num,date
-            if file.startswith('amq_ranked_central_'):
-                offset = 19
-                central = True
-            elif file.startswith('amq_ranked_west_'):
-                offset = 16
-                central = False
-            else: assert 0, 'invalid file name format'
-            year = int(file[offset:offset+4])
-            season = int(file[offset+6:offset+8])
-            try:
-                num = int(file[offset+9:offset+11])
-                date = file[offset+12:offset+22]
-            except: # championship
-                assert file[offset+9:].startswith('champion')
-                num = -1
-                date = file[offset+18:offset+28]
-            obj = dict()
-            obj['region'] = 'central' if central else 'western'
-            obj['year'] = year
-            obj['season'] = season
-            obj['number'] = num
-            obj['date'] = date
-            obj['data'] = json.loads(open(dir+'/'+file).read())
-            data.append(obj)
+    
+    # process files in the directory
+    for file in filelist:
+        year,season,number,y,m,d,region = re_fname.fullmatch(file).groups()
+        obj = dict()
+        obj['region'] = region
+        obj['year'] = int(year)
+        obj['season'] = int(season)
+        obj['number'] = -1 if number == 'ch' else int(number)
+        obj['date'] = '%s-%s-%s'%(y,m,d)
+        obj['data'] = json.loads(open('ranked_data/'+file,'r').read())
+        data.append(obj)
+    
+    if store_cached_obj:
+        pickle.dump(data,bz2.BZ2File('ranked_data.pickle.bz2','wb'))
+    
     return data
 
+def clean_ranked_data():
+    '''
+    Not implemented yet. Intention is to format the object returned from
+    read_ranked_data() consistently since the scraped JSON files might come from
+    different (versions of a) userscript(s) and structure their data a bit
+    differently. This might be challenging to get right and it might not be
+    feasible to handle every case of inconsistency.
+    '''
+
 if __name__ == '__main__':
-    data = get_data(True)
+    print('testing')
+    data = read_ranked_data(True)
+    print(len(data),'ranked files loaded')
+    print(data[0])
+    print('done')
+
